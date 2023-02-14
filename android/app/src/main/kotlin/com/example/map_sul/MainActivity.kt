99b17fc1742e15b429b1644cba184b123059a3e9
Thilosha Nipunajith <66148903+Thilosha@users.noreply.github.com>
2023-02-14 10:06:36 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/map_sul/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.map_sul
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:map_sul/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+
